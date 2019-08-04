f9ae59121a28594a91f04ffff8ce8ffafe06916a
Juan Pablo <jotapdiez@gmail.com>
2019-08-04 18:06:55 -0300

    Updates

---
+++ precache-manifest.8d7e2c0e8de806c1ea5de8124ea36911.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d37a45457f440894c437e5b446ca40",
+    "url": "/react1010/index.html"
+  },
+  {
+    "revision": "2401fe8a61eaea5143a4",
+    "url": "/react1010/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "044b5e1f2650ce0f1c3a",
+    "url": "/react1010/static/css/main.5f32eac2.chunk.css"
+  },
+  {
+    "revision": "2401fe8a61eaea5143a4",
+    "url": "/react1010/static/js/2.f0789e58.chunk.js"
+  },
+  {
+    "revision": "044b5e1f2650ce0f1c3a",
+    "url": "/react1010/static/js/main.0b6b1ea5.chunk.js"
+  },
+  {
+    "revision": "4d0cc928fdb5633d9d31",
+    "url": "/react1010/static/js/runtime~main.ee48133a.js"
+  }
+]);
